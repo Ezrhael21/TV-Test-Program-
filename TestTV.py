0d2bbc498eabaa32a59dc34c76a64ae9f920fabb
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-05-15 17:44:11 +0800

    Pseudocode

--- TestTV.py
+++ TestTV.py
@@ -3,3 +3,18 @@
 # 05/15/2023
 # TV Test Program
 
+# Create a seperate file class for the following:
+# Initialize the TV object
+# Method to turn on the tv
+# Method to turn of the tv
+# Method to get channel from the user
+# Method to set channel
+# Method to get volume from the user
+# Method to set volume
+# Method to increase channel by 1
+# Method to decrease channel by 1
+# Method to increase volume by 1 
+# Method to decrease volume by 1 
+# Method to print the output
+# Call the class from the main file
+# Display the output
