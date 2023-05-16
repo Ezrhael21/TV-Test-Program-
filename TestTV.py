30b54a1e463c29dea469c18bcebf243fdebf2f6d
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-05-16 23:30:01 +0800

    Create the same code for TV2 Details

--- TestTV.py
+++ TestTV.py
@@ -24,9 +24,20 @@ if status_one.lower() == "yes":
 else:
     tv_one.turn_off()
 
+# Ask the user for TV2 details
+channel_two = int(input("Enter TV2's channel: "))
+volume_level_two = int(input("Enter TV2's volume level: "))
+status_two = input("Is TV2 on? (yes/no): ")
+
 # Set TV2 Details
-tv_two.set_channel(4)
-tv_two.set_volumelevel(9)
+tv_two.set_channel(channel_two)
+tv_two.set_volumelevel(volume_level_two)
+
+# Turn on/off TV1 based on user input
+if status_two.lower() == "yes":
+    tv_two.turn_on()
+else:
+    tv_two.turn_off()
 
 # Display the output
 print("TV1's channel is " + str(tv_one.get_channel()) + " and volume level is " + str(tv_one.get_volumelevel()))
