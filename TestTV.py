f440acad1f6ddda0f24703a240a4f2702b19d125
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-05-16 23:19:28 +0800

    Set TV1 Details

--- TestTV.py
+++ TestTV.py
@@ -9,4 +9,10 @@ from TV import TVTest
 tv_one = TVTest()
 tv_two = TVTest()
 
+# Set TV1 Details
+tv_one.set_channel(3)
+tv_one.set_volumelevel(5)
+
+
 # Display the output
+print("TV1's channel is " + str(tv_one.get_channel()) + " and volume level is " + str(tv_one.get_volumelevel()))
