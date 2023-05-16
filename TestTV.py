3eed8da93f65104397b8acab3534ce71261bcd9a
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-05-16 23:14:47 +0800

    Create two seperate instances of the TVTest Class

--- TestTV.py
+++ TestTV.py
@@ -5,32 +5,8 @@
 
 from TV import TVTest
 
-tv = TVTest()
+# Create two seperate instances of the TVTest Class
+tv_one = TVTest()
+tv_two = TVTest()
 
-print (tv.channel)
-print (tv.volumelevel)
-print (tv.on)
-
-tv.set_channel(5)
-tv.set_volumelevel(3)
-tv.channel_down()
-tv.volumelevel_down()
-
-print (tv.get_channel())
-print (tv.get_volumelevel())
-
-
-# Create a seperate file class for the following:
-# Method to turn on the tv
-# Method to turn of the tv
-# Method to get channel from the user
-# Method to set channel
-# Method to get volume from the user
-# Method to set volume
-# Method to increase channel by 1
-# Method to decrease channel by 1
-# Method to increase volume by 1 
-# Method to decrease volume by 1 
-# Method to print the output
-# Call the class from the main file
 # Display the output
