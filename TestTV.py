f9ea473615dc075c7413530682c3ce4aa5cb3c80
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-05-16 23:24:36 +0800

    Ask the user for TV1 Details

--- TestTV.py
+++ TestTV.py
@@ -9,14 +9,17 @@ from TV import TVTest
 tv_one = TVTest()
 tv_two = TVTest()
 
+# Ask the user for TV1 details
+channel_one = int(input("Enter TV1's channel: "))
+volume_level_one = int(input("Enter TV1's volume level: "))
+
 # Set TV1 Details
-tv_one.set_channel(3)
-tv_one.set_volumelevel(5)
+tv_one.set_channel(channel_one)
+tv_one.set_volumelevel(volume_level_one)
 
 # Set TV2 Details
 tv_two.set_channel(4)
 tv_two.set_volumelevel(9)
-
 
 # Display the output
 print("TV1's channel is " + str(tv_one.get_channel()) + " and volume level is " + str(tv_one.get_volumelevel()))
